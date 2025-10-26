648dc303604b3742b5e9423adf4e85ab92ed0d54
Kismet <hello@kismetdursun.com>
2025-10-25 19:17:32 -0700

    Fix Next.js build with canvas external

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   experimental: {
     typedRoutes: true
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("@napi-rs/canvas");
+    }
+
+    return config;
   }
 };
 
